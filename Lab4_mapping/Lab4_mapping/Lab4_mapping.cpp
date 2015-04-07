40e2f1c4a79ee2c7795a29f242bc878242ddf0b8
Stas <stas249501@gmail.com>
2015-04-07 20:29:50 +0300

    вроде как работает 5 задание

--- Lab4_mapping/Lab4_mapping/Lab4_mapping.cpp
+++ Lab4_mapping/Lab4_mapping/Lab4_mapping.cpp
@@ -1,7 +1,10 @@
 #include "stdafx.h"
 #include "MyMain.h"
 #include "CRC.h"
+#include <string>
 
+
+using namespace std;
 
 BOOL CtrlHandler(DWORD fdwCtrlType)
 {
@@ -20,39 +23,41 @@ int _tmain(int argc, _TCHAR* argv[])
 	SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
 	if (!StartMailBox())
 		return 0;
-	_tprintf(_T("Для работы с почтовым ящиком введите номер предложенной команды:\n"));
+	_tprintf_s(_T("Для работы с почтовым ящиком введите номер предложенной команды:\n"));
 	size_t c = -1;
 	while (c != 0){
-		_tprintf(_T("0. Выйти из программы.\n"));
-		_tprintf(_T("1. Добавить новое сообщение.\n"));
-		_tprintf(_T("2. Просмотреть сообщение.\n"));
-		_tprintf(_T("3. Удалить сообщение.\n"));
-		_tprintf(_T("4. Очистить почтовый ящик\n"));
-		_tprintf(_T("5. Просмотреть информацию о почтовом ящике.\n"));
+		_tprintf_s(_T("0. Выйти из программы.\n"));
+		_tprintf_s(_T("1. Добавить новое сообщение.\n"));
+		_tprintf_s(_T("2. Просмотреть сообщение.\n"));
+		_tprintf_s(_T("3. Удалить сообщение.\n"));
+		_tprintf_s(_T("4. Очистить почтовый ящик\n"));
+		_tprintf_s(_T("5. Просмотреть информацию о почтовом ящике.\n"));
 		_tscanf_s(_T("%d"), &c);
+		string tempBuf;
+		TCHAR* buf;
 		switch (c)
 		{
 		case 0:
 			ExitFromMailBox();
 			break;
 		case 1:
-			_tprintf(_T("Введите текст сообщения:\n"));
-			char temp[501];
+			_tprintf_s(_T("Введите текст сообщения:\n"));
 			fflush(stdin);
-			scanf("%[^\r\n]", &temp);
-			TCHAR message[501];
-			OemToChar(temp, message);
-			addNewMessage(message);
+			getline(std::cin, tempBuf);
+			buf = new TCHAR[tempBuf.size() + 1];
+			OemToChar(tempBuf.c_str(), buf);
+			addNewMessage(buf);
+			delete[] buf;
 			break;
 		case 2:
-			_tprintf(_T("Введите индекс сообщения:\n"));
+			_tprintf_s(_T("Введите индекс сообщения:\n"));
 			int index1;
 			fflush(stdin);
 			scanf("%d", &index1);
 			ReadMessage(index1);
 			break;
 		case 3:
-			_tprintf(_T("Введите индекс для удаления:\n"));
+			_tprintf_s(_T("Введите индекс для удаления:\n"));
 			int index;
 			fflush(stdin);
 			scanf("%d", &index);
@@ -67,7 +72,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 
 	}
-	_tprintf(_T("Пока\n"));
+	_tprintf_s(_T("Пока\n"));
 	//system("pause");
 	return 0;
 }
