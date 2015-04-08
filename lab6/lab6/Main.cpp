84e19e98843165b5512a8e86c3c1552631532cbc
Stas <stas249501@gmail.com>
2015-04-09 00:05:47 +0300

    поправил некоторые ошибки в моем лру

--- lab6/lab6/Main.cpp
+++ lab6/lab6/Main.cpp
@@ -7,20 +7,21 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	//getMemoryInfo();
 	//theLeastSufficientAdd((SIZE_T)4096);
-	/*theLeastSufficientDelete(5);
+	theLeastSufficientDelete(5);
 	//swapPages(6);
 	SIZE_T mas[] = { 100, 541, 6544, 100, 300, 7000, 1333, 1333, 100 };
 
 	for (int i = 0; i < sizeof(mas)/sizeof(SIZE_T); i++)
 	{
 		addToPages(mas[i]);
-	}*/
+	}
 
 	int myM[] = { 0, 1, 0, 2, 3, 0, 3, 1 };
+	int size = sizeof(myM) / sizeof(int);
 
-	for (int i = 0; i < sizeof(myM) / sizeof(int); i++)
+	for (int i = 0; i < size; i++)
 	{
-		put(i);
+		put(myM[i]);
 		printCache();
 	}
 
