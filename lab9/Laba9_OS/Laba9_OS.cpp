37d32ca4007bc5a57acfd4aa6e9ab2275350aa46
Anna Hunko <annhv@yandex.ru>
2015-05-20 23:40:52 +0300

    nulls

--- lab9/Laba9_OS/Laba9_OS.cpp
+++ lab9/Laba9_OS/Laba9_OS.cpp
@@ -6,14 +6,25 @@
 #include "TheThreadFunc.h"
 #include <vector>
 #include <mutex>
+#include <locale.h>
 
 #define MyCreateThread(threadFunc, id) CreateThread(0, 0, threadFunc, 0, 0, id);
+#define CreateUnsuspendedProcess(path, lpsi, lppi, directory) CreateProcess(0, path, 0, 0, 0, 0, 0, directory, lpsi, lppi);
+
 
 HANDLE hThread[10];
 DWORD id[10];
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	_tsetlocale(LC_ALL, _T("Russian"));
+	STARTUPINFO si = { 0 };
+	PROCESS_INFORMATION pi;
+	si.cb = sizeof(STARTUPINFO);
+	TCHAR temp[256];
+	BOOL b;
+	TCHAR ProcName[] = _T("Process.exe");
+
 #ifdef _DEBUG
 	_tprintf(_T("debug\n"));
 #else
@@ -23,9 +34,23 @@ int _tmain(int argc, _TCHAR* argv[])
 	HANDLE hMap = CreateFileMapping(INVALID_HANDLE_VALUE, 0, PAGE_READWRITE, 0, sizeof(long), _T("CountCall"));
 	long* i = (long *)MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
 	*i = 0;
-	for (int j = 0; j < 10; j++) {
+	/*for (int j = 0; j < 10; j++) {
 		hThread[j] = MyCreateThread(ThreadFunc, id + j);
+	}*/
+
+
+	for (int j = 0; j < 10; j++) {
+		//hThread[j]
+		b = CreateUnsuspendedProcess(ProcName, &si, &pi, 0);
+		if (!b)
+		{
+			_tprintf(_T("Не удалось запустить процесс %s"));
+			return -1;
+		}
+		hThread[j] = pi.hProcess;
+		//_stprintf_s(temp, _T("%s %d"), hThread[j], j);
 	}
+
 
 	WaitForMultipleObjects(10, hThread, true, INFINITE);
 
--- lab9/Process/Process.cpp
+++ lab9/Process/Process.cpp
@@ -3,13 +3,14 @@
 
 #include "stdafx.h"
 #include <Windows.h>
-#include "TheThreadFunc.h"
+#include "..\Laba9_OS\TheThreadFunc.h"
 
-#define MyCreateThread(threadFunc, id) CreateThread(0, 0, threadFunc, 0, 0, id);
+#define MyCreateThread(threadFunc) CreateThread(0, 0, threadFunc, 0, 0, 0);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	HANDLE hThread = MyCreateThread(ThreadFunc, id);
+	//_tprintf(_T("1Procces stars!\n "));//,argv[1]);
+	HANDLE hThread = MyCreateThread(ThreadFunc);
 	WaitForSingleObject(hThread, INFINITE);
 	CloseHandle(hThread);
 	return 0;
